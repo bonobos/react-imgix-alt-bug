52459c2f880e07011be4b9a4f1546f91b3c367ae
Jack Chu <jack@jackchu.com>
2016-12-01 11:39:07 -0500

    Create repo to demonstrate react-imgix width bug with Firefox and Edge 14

---
+++ src/App.js
@@ -0,0 +1,37 @@
+import React, { Component } from 'react';
+import logo from './logo.svg';
+import ReactImgix from "react-imgix"
+import './App.css';
+
+class App extends Component {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <h2>ReactImgix alt prop bug</h2>
+        </div>
+        <p className="App-intro">
+          On Edge 14 and Firefox, if you pass an alt tag, react-imgix constrains the width to 100px. If you remove the alt tag, it sets it to 1px.
+        </p>
+        <p className="App-bug">
+          <h3>Without alt tag</h3>
+          <ReactImgix
+            src="https://bonobos-prod-s3.imgix.net/products/20820/original/PNT_WC_Cappuchinos_Slim_hero1a.jpg"
+            imgProps={{
+              alt: "This is alt text",
+            }}
+          />
+        </p>
+        <p className="App-bug">
+          <h3>With alt tag</h3>
+          <ReactImgix
+            src="https://bonobos-prod-s3.imgix.net/products/20820/original/PNT_WC_Cappuchinos_Slim_hero1a.jpg"
+          />
+        </p>
+      </div>
+    );
+  }
+}
+
+export default App;
